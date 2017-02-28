fc2adced4675a5935205dee4eb8ae417d1d4d356
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-02-28 15:53:28 +0000

    First go

--- Week5/PHPIntro/Aberdeen.php
+++ Week5/PHPIntro/Aberdeen.php
@@ -7,7 +7,22 @@
 <body>
 <p>
     <?php
-    echo "Hello world!";
+    $name="Shaun";
+    $myage="17";
+
+    print"Hello " . $name;
+    print"Your age is " . $myage;
+    if($myage>"21"){
+        print "You can buy sausage rolls";
+    }
+    elseif($myage>"18"){
+        print "You can buy mugs";
+    }
+    elseif($myage>"16"){
+        print "You can buy specs";
+    }
+    else
+    print "You can't buy anything";
     ?>
 </p>
 </body>
