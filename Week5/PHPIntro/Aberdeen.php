73637e051e6094dbb472da4f7ca9c5c955acda4f
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-02-28 15:42:15 +0000

    First go

---
+++ Week5/PHPIntro/Aberdeen.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aberdeen Folk</title>
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello world!";
+    ?>
+</p>
+</body>
+</html>
