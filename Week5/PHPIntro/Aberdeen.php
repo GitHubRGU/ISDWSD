5c8f53202b6ff4d234022ccfab5aa8f86a989430
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-02-28 16:09:12 +0000

    First go

--- Week5/PHPIntro/Aberdeen.php
+++ Week5/PHPIntro/Aberdeen.php
@@ -10,8 +10,8 @@
     $name="Shaun";
     $myage="12";
 
-    print"Hello " . $name;
-    print"Your age is " . $myage;
+    print "Hello " . $name;
+    print "Your age is " . $myage;
     if($myage>"21"){
         print "You can buy sausage rolls";
     }
@@ -25,5 +25,22 @@
     print "You can't buy anything";
     ?>
 </p>
+
+<p>
+    <?php
+    $wantedgood="mugs";
+
+    switch($wantedgood){
+    case mugs:
+        echo "You have to be 18 to buy mugs";
+        break;
+    case specs:
+        echo "You need to be 16 to buy specs";
+        break;
+    case sausagerolls:
+        echo "You need to be 21 to buy sausage rolls";
+    />
+</p>
+
 </body>
 </html>
