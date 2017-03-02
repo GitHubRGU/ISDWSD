0974dc05efa87b4d6103bb28a62704f773effe2c
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-03-02 13:10:01 +0000

    First go

--- Week5/PHPIntro/Aberdeen.php
+++ Week5/PHPIntro/Aberdeen.php
@@ -90,7 +90,7 @@
     for($day=1;$day<32;$day++)
     {
         print "On the " . $day . " of the month ";
-        if($day>"1"){
+        if($day=="1"){
             print "no products";
         }
         elseif($day/2==$day/2){
