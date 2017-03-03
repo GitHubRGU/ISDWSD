9adc4c10316bb470f0c5c4dd32c7d337dc81dbad
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-03-03 12:36:18 +0000

    First go

--- Week5/PHPIntro/Aberdeen.php
+++ Week5/PHPIntro/Aberdeen.php
@@ -172,7 +172,7 @@
     // Activity 7:
 
     function calcaward($specs,$mugs,$srolls){
-        return(($specs * $mugs * $srolls) * ($specs * $mugs * $srolls));
+        return(10 * (($specs * $mugs * $srolls) * ($specs * $mugs * $srolls)) / 2);
     }
 
     function wanted($name,$specs,$mugs,$srolls){
