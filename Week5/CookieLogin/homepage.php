5a35b179cebaf6c67d21a459ae27a25ba3ddd453
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-03-03 14:46:26 +0000

    First go

---
+++ Week5/CookieLogin/homepage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1615309
+ * Date: 03/03/2017
+ * Time: 14:38
+ */
+
+$username="Mike";
+$password="mysecretpassword";
+
+print "Triggered the php!!!";
+print ":-)";
