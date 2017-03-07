44a1a9f2c1803fbc048ea424405d9009d41966f1
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-03-07 15:16:28 +0000

    First go

--- Week5/CookieLogin/homepage.php
+++ Week5/CookieLogin/homepage.php
@@ -18,14 +18,5 @@ print $password;
 
 
 
-set cookie(‘access_level’,'standarduser');
+?>
 
-function displayAccessLevelInformation(accessLevel){
-    if($access_level=="standarduser"){
-        echo "<p>You are currently logged in as a standard user</p>";
-    }
-    elseif($access_level=="root"){
-        echo "<p>You are currently logged in as a root user</p>";
-        echo "<p>You now have access to additional administrative features</p>";
-    }
-}
