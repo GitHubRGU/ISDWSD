54998a5c84a802962d756d98bfdcd39752e639fa
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-03-09 14:16:45 +0000

    First go

--- Week5/SelfRef/helloSelector.php
+++ Week5/SelfRef/helloSelector.php
@@ -22,10 +22,13 @@
 
 <main>
     <br>
+    <h3>Plain links:</h3>
     <p><a href="helloPrinter.php">Earth</a></p>
     <p><a href="helloPrinter.php">Mars</a></p>
     <p><a href="helloPrinter.php">Uranus</a></p>
+    <br>
 
+    <h3>Extended links:</h3>
     <p><a href="helloPrinter.php?query=Earth&type=planet">Earth</a></p>
     <p><a href="helloPrinter.php?query=Mars&type=planet">Mars</a></p>
     <p><a href="helloPrinter.php?query=Uranus&type=planet">Uranus</a></p>
