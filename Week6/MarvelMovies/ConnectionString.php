d0c739bddb30b31ce74132a5f0140f7d544dd796
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-13 13:38:51 +0100

    Trying to connect to Azure mySQL

--- Week6/MarvelMovies/ConnectionString.php
+++ Week6/MarvelMovies/ConnectionString.php
@@ -21,19 +21,28 @@ $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_d
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    print "<p></p>";
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    print "<p></p>";
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    print "<p></p>";
     exit;
 }
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+print "<p></p>";
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+print "<p></p>";
 
-echo $connectstr_dbhost;
-echo $connectstr_dbusername;
-echo $connectstr_dbpassword;
-echo $connectstr_dbname;
 
+echo "dbhost: " . $connectstr_dbhost;
+print "<p></p>";
+echo "username: " . $connectstr_dbusername;
+print "<p></p>";
+echo "dbpassword:" . $connectstr_dbpassword;
+print "<p></p>";
+echo "dbname:" . $connectstr_dbname;
+print "<p></p>";
 
 mysqli_close($link);
 
