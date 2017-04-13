adce8cf415c187f447f6b5abaa3e7148478889e9
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-13 11:15:39 +0100

    Trying to connect to Azure mySQL

--- Week6/MarvelMovies/ConnectionString.php
+++ Week6/MarvelMovies/ConnectionString.php
@@ -1,9 +1,13 @@
 <?php
 
+echo "Part 1";
+
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
+
+echo "Part 2";
 
 foreach ($_SERVER as $key => $value) {
     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
