d1bd80f04fc203efb2642c0b3fbc47b07a8744d5
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-14 11:15:30 +0100

    Login system testing

--- Sprong/login.php
+++ Sprong/login.php
@@ -1,9 +1,9 @@
 <?php
 
-//  Establish connection to Azure mySQL database ($link):
+//  Establish connection to my (Sprong) Azure mySQL database ($link):
 include("connection.php");
 
-
+//  Check if the username and password fields are blank:
 if(empty($_POST["username"]) || empty($_POST["password"]))
 {
     echo "Both fields are required.";
@@ -11,17 +11,21 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
 {
     $username=$_POST['username'];
     $password=$_POST['password'];
+
+    //  SQL query to select uid for the user that matches the username and password entered
+    //  and store it in $result:
     $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
     $result=mysqli_query($link,$sql);
 
+    //  Check how many rows are in result (should only be one):
     if(mysqli_num_rows($result == 1))
     {
         //  If query doesn't return a single row, then something is wrong,
-        //  send the user to the home page:
+        //  so send the user to the home page:
         header("location: home.php");
     }else
     {
-        echo "Incorrect username or password.";
+        echo "Incorrect username or password entered!";
     }
 }
 
