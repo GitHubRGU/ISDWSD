2fa24f8364991d090f81ee9863563f13d5c6fb83
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-15 12:18:27 +0100

    Login system testing

--- Sprong/index.php
+++ Sprong/index.php
@@ -37,27 +37,27 @@ foreach ($rules as $action => $rule) {
 ?>
 
 
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Login to Sprong</title>
-    <link rel="stylesheet" href="CSS/style.css" type="text/css" />
-</head>
-
-<body>
-<h1>Sprong Login (form with Session)</h1>
-<div class="loginBox">
-    <h3>Login to Sprong</h3>
-    <br><br>
-    <form method="post" action="login.php">
-        <label>Username:</label><br>
-        <input type="text" name="username" placeholder="username"/><br><br>
-        <label>Password:</label><br>
-        <input type="password" name="password" placeholder="password"/><br><br>
-        <input type="submit" name="submit" value="login"/>
-    </form>
-    <div class="error"><?php // echo error;?><?php // echo $username; echo $password?></div>
-</div>
-</body>
-</html>
+<!--<!doctype html>-->
+<!--<html>-->
+<!--<head>-->
+<!--    <meta charset="utf-8">-->
+<!--    <title>Login to Sprong</title>-->
+<!--    <link rel="stylesheet" href="CSS/style.css" type="text/css" />-->
+<!--</head>-->
+<!---->
+<!--<body>-->
+<!--<h1>Sprong Login (form with Session)</h1>-->
+<!--<div class="loginBox">-->
+<!--    <h3>Login to Sprong</h3>-->
+<!--    <br><br>-->
+<!--    <form method="post" action="login.php">-->
+<!--        <label>Username:</label><br>-->
+<!--        <input type="text" name="username" placeholder="username"/><br><br>-->
+<!--        <label>Password:</label><br>-->
+<!--        <input type="password" name="password" placeholder="password"/><br><br>-->
+<!--        <input type="submit" name="submit" value="login"/>-->
+<!--    </form>-->
+<!--    <div class="error">--><?php //// echo error;?><!----><?php //// echo $username; echo $password?><!--</div>-->
+<!--</div>-->
+<!--</body>-->
+<!--</html>-->
--- Sprong/login.php
+++ Sprong/login.php
@@ -1,4 +1,66 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    include("header.php");
+    ?>
+
+    <main>
+        <form action="login" method="post">
+            <input type="text" name="username" placeholder="username"></br>
+            <input type="password" name="password" placeholder="password"></br>
+            <p><input type="submit" value="Submit"></p>
+        </form>
+    </main>
+
+    <?php
+    include("footer.php");
+
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    include("connection.php");
+
+    $username = $_POST["username"];
+    $password = $_POST{"password"};
+
+    function checklogin($username, $password, $link)
+    {
+        $sql = "SELECT * FROM users WHERE username='" . $username . "' and password='" . $password . "'";
+        $result = $link->query($sql);
+        while ($row = $result->fetch_array()) {
+            return true;
+        }
+        return false;
+    }
+
+    if (checklogin($username, $password, $link)) {
+        session_start();
+        $_SESSION['username'] = $username;
+        header("location:./");
+    } else {
+        header("location:login");
+    }
+
+} else {
+    print('something has gone VERY wrong!');
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //  Establish connection (called $link) to my Sprong Azure mySQL database:
 include("connection.php");
@@ -8,36 +70,36 @@ include("connection.php");
 //  Check if the username and password fields are blank:
 if(empty($_POST["username"]) || empty($_POST["password"]))
 {
-    echo "Both fields are required.";
+echo "Both fields are required.";
 }else
 {
-    $username=$_POST['username'];
-    $password=$_POST['password'];
+$username=$_POST['username'];
+$password=$_POST['password'];
 
-    echo "Username: " . $username . "<p></p>";
-    echo "Password: " . $password . "<p></p>";
+echo "Username: " . $username . "<p></p>";
+echo "Password: " . $password . "<p></p>";
 
-    //  SQL query to select uid for the user that matches the username and password entered
-    //  and store it in $result:
-    //  $sql="SELECT uid FROM users WHERE username='$username' AND password='$password'";
-    $sql="SELECT * FROM users";
-    $result=mysqli_query($link,$sql);
+//  SQL query to select uid for the user that matches the username and password entered
+//  and store it in $result:
+//  $sql="SELECT uid FROM users WHERE username='$username' AND password='$password'";
+$sql="SELECT * FROM users";
+$result=mysqli_query($link,$sql);
 
-    //  What is $result returning?:
-    echo "This is what result is returning: " . "<p></p>";
-    var_dump($result);
-    echo "End of result." . "<p></p>";
+//  What is $result returning?:
+echo "This is what result is returning: " . "<p></p>";
+var_dump($result);
+echo "End of result." . "<p></p>";
 
-    //  Check how many rows are in result (should only be one):
-    if(mysqli_num_rows($result) == 1)
-    {
-        //  If query doesn't return a single row, then something is wrong,
-        //  so send the user to the home page:
-        header("location: home.php");
-    }else
-    {
-        echo "Incorrect username or password entered!";
-    }
+//  Check how many rows are in result (should only be one):
+if(mysqli_num_rows($result) == 1)
+{
+//  If query doesn't return a single row, then something is wrong,
+//  so send the user to the home page:
+header("location: home.php");
+}else
+{
+echo "Incorrect username or password entered!";
+}
 }
 
 
