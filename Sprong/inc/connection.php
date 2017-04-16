6fa1859cef77822861f00e03df3fa7103787ec53
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-16 09:40:58 +0100

    Login system testing

--- Sprong/inc/connection.php
+++ Sprong/inc/connection.php
@@ -44,6 +44,46 @@ print "<p></p>";
 echo "dbname: " . $connectstr_dbname;
 print "<p></p>";
 
+
+
+
+echo "<p>Step A</p>";
+// create an SQL query as a string
+$sql_query = "SELECT * FROM users";
+
+echo "<p>Step B</p>";
+// execute the SQL query
+$result = $link->query($sql_query);
+
+echo "<p>Step C</p>";
+// We can then process the results from this (step 4)
+// iterate over $result object one $row at a time
+// use fetch_array() to return an associative array
+// process the $row
+echo "<p>Query: " . $sql_query . "</p>";
+echo "<p>Result: " . $result . "</p>";
+while($row = $result->fetch_array()){
+    // print out fields from row of data
+    echo "<p>".$row."</p>";
+}
+
+echo "<p>Step D</p>";
+$result->close();
+
+echo "<p>Step E</p>";
+// close connection to database
+$link->close();
+
+echo "<p>Step F</p>";
+
+
+
+
+
+
+
+
+
 mysqli_close($link);
 
 ?>
