45e5b9cb787bc53cd2e90d5aafa75b2721a21b3e
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-16 19:57:15 +0100

    Login system testing

--- Sprong/index.php
+++ Sprong/index.php
@@ -45,20 +45,11 @@ foreach ($rules as $action => $rule) {
 echo "
 <main>
 
-<p>Welcome to Sprong, the home of quality guitar maintenance and repair.</p>
-<p>Your gateway to a perfectly set up instrument!</p>
-<p>Sprong employ a team of three experienced luthiers, offering the following services:</p>
-<ul>
-  <li>Restring</li>
-  <li>Change of string gauge</li>
-  <li>Full instrument setup, to customers requirements / specifications</li>
-  <li>Instrument repairs</li>
-  <li>Clean up and restoration</li>
-  <li>Replacement of hardware / electronics</li>
-  <li>Fault finding and problem resolution</li>
-</ul> 
-<p>If you'd like us to undertake any of the above work on your instrument, please contact us!</p>
-<p>To view the current status of your existing work order, please log in using the link above.</p>
+<p>Welcome back to Sprong, the home of quality guitar maintenance and repair.</p>
+<p></p>
+<p>This is where you can check on the history and status of your requested work.</p>
+<p></p>
+<p>To view the current status of your existing work order, please use the View / Update link above.</p>
 
 
 </main>
