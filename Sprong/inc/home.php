96046fa6e946f6d36c77313b93c874b068bc1417
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-16 17:45:01 +0100

    Login system testing

--- Sprong/inc/home.php
+++ Sprong/inc/home.php
@@ -24,6 +24,10 @@ echo "
   <li>Replacement of hardware / electronics</li>
   <li>Fault finding and problem resolution</li>
 </ul> 
+<p>If you'd like us to undertake any of the above work on your instrument, please contact us!</p>
+<p>To view the current status of your existing work order, please log in using the link above.</p>
+
+
 </main>
 ";
 
