cc74e145461493b7d3159e378dae7321052eef20
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-16 17:53:44 +0100

    Login system testing

--- Sprong/inc/header.php
+++ Sprong/inc/header.php
@@ -17,11 +17,12 @@ session_start();
     <nav>
         <ul>
             <li><a href="../inc/home.php">Home Page</a></li>
-            <li><a href="../inc/viewJobStatus.php">View / update work progress</a></li>
+            <li><a href="../inc/contact.php">Contact us</a></li>
             <?php
             if (isset($_SESSION['username'])) {
                 //  User successfully logged in, so show appropriate links:
                 echo "<li><a href='../inc/requestWork.php'>Request work</a></li>";
+                echo "<li><a href='../inc/viewJobStatus.php'\">View / update work progress</a></li>"
                 echo "<li><a href='../inc/logout.php'>Logout</a></li>";
             } else {
                 //  User is not logged in, so display a login link:
