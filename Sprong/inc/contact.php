d9a54e31a5bc4c24ba7a222f4d60c297e575c813
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-16 17:51:39 +0100

    Login system testing

---
+++ Sprong/inc/contact.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="../CSS/style.css" type="text/css" />
+<body>
+
+
+<?php
+
+include("../inc/header.php");
+
+echo "
+<main>
+<p>(contact.php)</p>
+<p>Welcome to Sprong, the home of quality guitar maintenance and repair.</p>
+<p>Your gateway to a perfectly set up instrument!</p>
+<p></p>
+<p><h4>Contact us</h4></p>
+<p></p>
+<p><h2>Address</h2></p>
+<p>1 Union Street</p>
+<p>Aberdeen</p>
+<p>AB1 1BA</p>
+<p></p>
+<p>Telephone: (01224) 123456</p>
+<p></p>
+<p>email: enquiries@spring.com</p>
+
+
+</main>
+";
+
+include("../inc/footer.php");
+
+?>
+
+
+</body>
+</html>
+
+
