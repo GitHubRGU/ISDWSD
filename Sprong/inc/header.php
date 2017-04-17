49279293f53460ac12cb81e13779f0b5890a9d52
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-17 12:12:47 +0100

    Login system testing

--- Sprong/inc/header.php
+++ Sprong/inc/header.php
@@ -16,17 +16,19 @@ session_start();
     <h1>Sprong - Guitar maintenance and repairs</h1>
     <nav>
         <ul>
-            <li><a href="../inc/home.php">Home Page</a></li>
-            <li><a href="../inc/contact.php">Contact us</a></li>
             <?php
             if (isset($_SESSION['username'])) {
-                //  User successfully logged in, so show appropriate links:
+                //  User successfully logged in, so show all appropriate links:
+                echo "<li><a href='home.php''>Home Page</a></li>";
+                echo "<li><a href='contact.php'>Contact us</a></li>";
                 echo "<li><a href='manageUsers.php'>View/update Customer Details</a></li>";
                 echo "<li><a href='requestWork.php'>Request work</a></li>";
                 echo "<li><a href='viewJobStatus.php'>View / update work progress</a></li>";
                 echo "<li><a href='logout.php'>Logout</a></li>";
             } else {
-                //  User is not logged in, so display a login link:
+                //  User is not logged in, so display basic links:
+                echo "<li><a href='../inc/home.php''>Home Page</a></li>";
+                echo "<li><a href='../inc/contact.php'>Contact us</a></li>";
                 echo "<li><a href='../inc/login.php'>Login</a></li>";
             }
             ?>
