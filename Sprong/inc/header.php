db40f6800fd5a937acbc696e00922fa02b376f1a
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-17 12:30:45 +0100

    Login system testing

--- Sprong/inc/header.php
+++ Sprong/inc/header.php
@@ -24,7 +24,7 @@ session_start();
                 echo "<li><a href='manageUsers.php'>View/update Customer Details</a></li>";
                 echo "<li><a href='requestWork.php'>Request work</a></li>";
                 echo "<li><a href='viewJobStatus.php'>View / update work progress</a></li>";
-                echo "<li><a href='logout.php'>Logout</a></li>";
+                echo "<li><a href='../inc/logout.php'>Logout</a></li>";
             } else {
                 //  User is not logged in, so display basic links:
                 echo "<li><a href='home.php''>Home Page</a></li>";
