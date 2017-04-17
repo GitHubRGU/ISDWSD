4ed78e72ea7b5227fee217260d6b49904494663a
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-17 13:19:18 +0100

    Login system testing

--- Sprong/inc/createUser.php
+++ Sprong/inc/createUser.php
@@ -20,21 +20,21 @@ echo "
                 <input type=\"text\" name=\"firstname\" placeholder=\"First name\"/><br><br>
                 <label>Surname:</label>
                 <input type=\"text\" name=\"surname\" placeholder=\"Surname\"/><br><br>
-                <label>Username:</label><br>
+                <label>Username:</label>
                 <input type=\"text\" name=\"username\" placeholder=\"Enter username here\"/><br><br>
-                <label>Password:</label><br>
+                <label>Password:</label>
                 <input type=\"password\" name=\"password\" placeholder=\"Enter password here\"/><br><br>
-                <label>Address line 1:</label><br>
+                <label>Address line 1:</label>
                 <input type=\"text\" name=\"address1\" placeholder=\"Address line 1\"/><br><br>
-                <label>Address line 2:</label><br>
+                <label>Address line 2:</label>
                 <input type=\"text\" name=\"address2\" placeholder=\"Address line 2\"/><br><br>
-                <label>Address line 3:</label><br>
+                <label>Address line 3:</label>
                 <input type=\"text\" name=\"address3\" placeholder=\"Address line 3\"/><br><br>
-                <label>Postcode:</label><br>
+                <label>Postcode:</label>
                 <input type=\"text\" name=\"postcode\" placeholder=\"Postcode\"/><br><br>
-                <label>Telephone:</label><br>
+                <label>Telephone:</label>
                 <input type=\"text\" name=\"telephone\" placeholder=\"Telephone\"/><br><br>
-                <label>Email:</label><br>
+                <label>Email:</label>
                 <input type=\"text\" name=\"email\" placeholder=\"Email\"/><br><br>
                 <input type=\"submit\" name=\"submit\" value=\"Create user\"/>
             </form>
