895ae12980fff178284986d14a3eb7f1271aee8b
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-17 13:05:14 +0100

    Login system testing

---
+++ Sprong/inc/createUser.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="../CSS/style.css" type="text/css" />
+<body>
+
+
+<?php
+
+include("header.php");
+
+echo "
+<main>
+<p>createUsers.php.</p>
+</main>
+";
+
+include("footer.php");
+
+?>
+
+
+</body>
+</html>
