19f36ade03a1b289d527090ad8937df4a923a267
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-17 13:44:59 +0100

    Login system testing

--- Sprong/inc/createUser.php
+++ Sprong/inc/createUser.php
@@ -17,41 +17,32 @@ echo "
 
 <table>
 
-<tr><td><label for=\"firstname\">First name:</label>                
-  <td><input type=\"text\" name=\"firstname\" id=\"firstname\" size=\"40\">
+<tr><td><label for=\"firstname\">First name:</label><td><input type=\"text\" name=\"firstname\" id=\"firstname\" size=\"40\">
 
-<tr><td><label for=\"surname\">Surname:</label>                
-  <td><input type=\"text\" name=\"surname\" id=\"surname\" size=\"40\">
+<tr><td><label for=\"surname\">Surname:</label><td><input type=\"text\" name=\"surname\" id=\"surname\" size=\"40\">
 
-<tr><td><label for=\"username\">Username:</label>                
-  <td><input type=\"text\" name=\"username\" id=\"username\" size=\"40\">
+<tr><td><label for=\"username\">Username:</label><td><input type=\"text\" name=\"username\" id=\"username\" size=\"40\">
   
-<tr><td><label for=\"password\">Password:</label>                
-  <td><input type=\"text\" name=\"password\" id=\"password\" size=\"40\">
+<tr><td><label for=\"password\">Password:</label><td><input type=\"text\" name=\"password\" id=\"password\" size=\"40\">
   
-<tr><td><label for=\"address1\">Address line 1:</label>                
-  <td><input type=\"text\" name=\"address1\" id=\"address1\" size=\"40\">
+<tr><td><label for=\"address1\">Address line 1:</label><td><input type=\"text\" name=\"address1\" id=\"address1\" size=\"40\">
     
-<tr><td><label for=\"address2\">Address line 2:</label>                
-  <td><input type=\"text\" name=\"address2\" id=\"address2\" size=\"40\">
+<tr><td><label for=\"address2\">Address line 2:</label><td><input type=\"text\" name=\"address2\" id=\"address2\" size=\"40\">
       
-<tr><td><label for=\"address3\">Address line 3:</label>                
-  <td><input type=\"text\" name=\"address3\" id=\"address3\" size=\"40\">
+<tr><td><label for=\"address3\">Address line 3:</label><td><input type=\"text\" name=\"address3\" id=\"address3\" size=\"40\">
 
-<tr><td><label for=\"postcode\">Postcode:</label>                
-  <td><input type=\"text\" name=\"postcode\" id=\"postcode\" size=\"40\">
+<tr><td><label for=\"postcode\">Postcode:</label><td><input type=\"text\" name=\"postcode\" id=\"postcode\" size=\"40\">
   
-<tr><td><label for=\"telephone\">Telephone:</label>                
-  <td><input type=\"text\" name=\"telephone\" id=\"telephone\" size=\"40\">
+<tr><td><label for=\"telephone\">Telephone:</label><td><input type=\"text\" name=\"telephone\" id=\"telephone\" size=\"40\">
   
-<tr><td><label for=\"email\">Email:</label>                
-  <td><input type=\"text\" name=\"email\" id=\"email\" size=\"40\">
+<tr><td><label for=\"email\">Email:</label><td><input type=\"text\" name=\"email\" id=\"email\" size=\"40\">
     
 </table>
 
 <br>            
 <input type=\"submit\" name=\"submit\" value=\"Create user\"/>
 </form>
+
 <div class=\"error\"><?php // echo error;?><?php // echo $username; echo $password?></div>
 
 
