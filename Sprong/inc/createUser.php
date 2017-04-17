70c06e5aa6781300d8aa3d3a22d8874cf8904b07
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-17 13:39:50 +0100

    Login system testing

--- Sprong/inc/createUser.php
+++ Sprong/inc/createUser.php
@@ -13,10 +13,8 @@ echo "
 <main>
 <p>To create your account, please enter your details below, click on the Create User button, then log in to Sprong.</p>
 
-            <form method=\"post\" action=\"createUser.php\"><br>
-            
-            
-            
+<form method=\"post\" action=\"createUser.php\"><br>
+
 <table>
 
 <tr><td><label for=\"firstname\">First name:</label>                
@@ -39,39 +37,21 @@ echo "
       
 <tr><td><label for=\"address3\">Address line 3:</label>                
   <td><input type=\"text\" name=\"address3\" id=\"address3\" size=\"40\">
-    
+
+<tr><td><label for=\"postcode\">Postcode:</label>                
+  <td><input type=\"text\" name=\"postcode\" id=\"postcode\" size=\"40\">
+  
+<tr><td><label for=\"telephone\">Telephone:</label>                
+  <td><input type=\"text\" name=\"telephone\" id=\"telephone\" size=\"40\">
+  
+<tr><td><label for=\"email\">Email:</label>                
+  <td><input type=\"text\" name=\"email\" id=\"email\" size=\"40\">
     
 </table>
             
-            
-            
-            
-            
-            
-            
-                <label>First name:</label>
-                <input type=\"text\" name=\"firstname\" placeholder=\"First name\"/><br>
-                <label>Surname:</label>
-                <input type=\"text\" name=\"surname\" placeholder=\"Surname\"/><br>
-                <label>Username:</label>
-                <input type=\"text\" name=\"username\" placeholder=\"Enter username here\"/><br>
-                <label>Password:</label>
-                <input type=\"password\" name=\"password\" placeholder=\"Enter password here\"/><br>
-                <label>Address line 1:</label>
-                <input type=\"text\" name=\"address1\" placeholder=\"Address line 1\"/><br>
-                <label>Address line 2:</label>
-                <input type=\"text\" name=\"address2\" placeholder=\"Address line 2\"/><br>
-                <label>Address line 3:</label>
-                <input type=\"text\" name=\"address3\" placeholder=\"Address line 3\"/><br>
-                <label>Postcode:</label>
-                <input type=\"text\" name=\"postcode\" placeholder=\"Postcode\"/><br>
-                <label>Telephone:</label>
-                <input type=\"text\" name=\"telephone\" placeholder=\"Telephone\"/><br>
-                <label>Email:</label>
-                <input type=\"text\" name=\"email\" placeholder=\"Email\"/><br><br>
-                <input type=\"submit\" name=\"submit\" value=\"Create user\"/>
-            </form>
-            <div class=\"error\"><?php // echo error;?><?php // echo $username; echo $password?></div>
+<input type=\"submit\" name=\"submit\" value=\"Create user\"/>
+</form>
+<div class=\"error\"><?php // echo error;?><?php // echo $username; echo $password?></div>
 
 
 </main>
