e4b4d0785ed957b7eb04de37a3c9f6d3bc9eda50
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-04-18 15:28:14 +0100

    Marvel Movies

--- Sprong/inc/manageUsers.php
+++ Sprong/inc/manageUsers.php
@@ -28,7 +28,7 @@ if (isset($_SESSION['username']))
 
 } else {
     //  User is not logged in, so send them to index.php:
-    header("location:./home.php");
+    header("location:../home.php");
 }
 
 
