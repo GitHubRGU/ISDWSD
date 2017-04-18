29b6d71e84cc0c3f3087a2c53cddc78468a0f91d
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-18 19:40:50 +0100

    manageUsers implementation

--- Sprong/inc/manageUsers.php
+++ Sprong/inc/manageUsers.php
@@ -10,43 +10,72 @@
 //  Establish connection to Azure mySQL database ($link):
 include("connection.php");
 
+include("header.php");
 
-session_start();
-if (isset($_SESSION['username']))
-{
-    //  Session has been set, so a user is logged in:
-    include("header.php");
-    echo "User is logged in!";
 
-    //  Build SQL query string to insert the new user into the database:
-    $username = $_SESSION['username'];
-    $sql_query="SELECT * FROM users WHERE username='" . $username . "'";
+if ($_POST) {
+    $firstname = $_POST["firstname"];
+    $surname = $_POST["surname"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $address1 = $_POST["address1"];
+    $address2 = $_POST["address2"];
+    $address3 = $_POST["address3"];
+    $postcode = $_POST["postcode"];
+    $telephone = $_POST["telephone"];
+    $email = $_POST["email"];
+
+//  Build SQL query string to insert the new user into the database:
+    $sql_query="INSERT INTO users (firstname, surname, username, password, address1, address2, address3, postcode, telephone, email) VALUES ('" . $firstname . "', '" . $surname . "', '" . $username . "', '" . $password . "', '" . $address1 . "', '" . $address2 . "', '" . $address3 . "', '" . $postcode . "', '" . $telephone . "', '" . $email . "')";
 
 //   DEBUGGING: Show me what the query string looks like:
-     echo "<p>SQL query string: " . $sql_query . "</p>";
+//    echo "<p>SQL query string: " . $sql_query . "</p>";
 
 //  Run the SQL query on the database:
     $result = mysqli_query($link,$sql_query);
-    $row = mysqli_fetch_assoc($result);
 
-    echo "Answer = " . $row['email'];
-
-
-
-    ?>
-
-<main>
-    <p>manageUsers.php.</p>
-</main>
-
-<?php
-    include("footer.php");
-
-} else {
-    //  User is not logged in, so send them to the home page:
-    header("location:../inc/home.php");
+//  Close the link to the mySQL database:
+    mysqli_close($link);
 }
 
+
+
+
+echo "
+<main>
+<p>To create your account, please enter your details below, click on the Create New Account button, then log in to Sprong.</p>
+
+<form method=\"post\" action=\"manageUsers.php\">
+
+<table>
+<tr><td><label for=\"firstname\">First name:</label><td><input type=\"text\" name=\"firstname\" id=\"firstname\" size=\"40\">
+<tr><td><label for=\"surname\">Surname:</label><td><input type=\"text\" name=\"surname\" id=\"surname\" size=\"40\">
+<tr><td><label for=\"username\">Username:</label><td><input type=\"text\" name=\"username\" id=\"username\" size=\"40\">
+<tr><td><label for=\"password\">Password:</label><td><input type=\"text\" name=\"password\" id=\"password\" size=\"40\">
+<tr><td><label for=\"address1\">Address line 1:</label><td><input type=\"text\" name=\"address1\" id=\"address1\" size=\"40\">
+<tr><td><label for=\"address2\">Address line 2:</label><td><input type=\"text\" name=\"address2\" id=\"address2\" size=\"40\">
+<tr><td><label for=\"address3\">Address line 3:</label><td><input type=\"text\" name=\"address3\" id=\"address3\" size=\"40\">
+<tr><td><label for=\"postcode\">Postcode:</label><td><input type=\"text\" name=\"postcode\" id=\"postcode\" size=\"40\">
+<tr><td><label for=\"telephone\">Telephone:</label><td><input type=\"text\" name=\"telephone\" id=\"telephone\" size=\"40\">
+<tr><td><label for=\"email\">Email:</label><td><input type=\"text\" name=\"email\" id=\"email\" size=\"40\">
+</table>
+
+<br>            
+<input type=\"submit\" name=\"submit\" value=\"Save updated details\"/><br>
+</form>
+
+
+</main>
+";
+
+//  Let the user know their account has been created:
+if ($_POST) {
+    echo "<p><h4>Account created!</h4></p>";
+    echo "<p>Please return to the home page and log in.</p>";
+}
+
+
+include("footer.php");
 
 ?>
 
