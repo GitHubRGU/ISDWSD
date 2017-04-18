ee5a4ece6302e3a078740d1c080f0b8d8ab395af
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-18 22:00:00 +0100

    manageUsers implementation

--- Sprong/inc/manageUsers.php
+++ Sprong/inc/manageUsers.php
@@ -108,8 +108,11 @@ echo "
 
 //  Let the user know their account details have been updated:
 if ($_POST) {
-    echo "<p><h4>Account details updated - click View/Update my details to confirm!</h4></p>";
-    header("location:../inc/manageUsers.php");
+    $message = "wrong answer";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+
+//    echo "<p><h4>Account details updated - click View/Update my details to confirm!</h4></p>";
+//    header("location:../inc/manageUsers.php");
 }
 
 
