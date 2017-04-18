1a90bff2a9fa8937f1b19bc8ed982eb0846ef126
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-04-18 14:49:30 +0100

    Marvel Movies

---
+++ Sprong/inc/workHistory.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="../CSS/style.css" type="text/css" />
+<body>
+
+
+<?php
+
+include("header.php");
+
+echo "
+<main>
+<p>workHistory.php.</p>
+</main>
+";
+
+include("footer.php");
+
+?>
+
+
+</body>
+</html>
