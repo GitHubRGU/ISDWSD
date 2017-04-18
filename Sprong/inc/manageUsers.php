cce730488ac40cfb7647b4341d7a734a092d2083
GitHubRGU <s.hyland2@rgu.ac.uk>
2017-04-18 15:54:08 +0100

    manageUsers

--- Sprong/inc/manageUsers.php
+++ Sprong/inc/manageUsers.php
@@ -17,6 +17,17 @@ if (isset($_SESSION['username']))
     //  Session has been set, so a user is logged in:
     include("header.php");
     echo "User is logged in";
+
+    //  Build SQL query string to insert the new user into the database:
+    $username=$_SESSION('username');
+    $sql_query="SELECT * FROM users WHERE username='" . $username . "'";
+
+//   DEBUGGING: Show me what the query string looks like:
+    echo "<p>SQL query string: " . $sql_query . "</p>";
+
+//  Run the SQL query on the database:
+    $result = mysqli_query($link,$sql_query);
+
     ?>
 
 <main>
