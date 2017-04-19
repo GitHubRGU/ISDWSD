ffdfc7cfd26801ead0df20cb8714dbe3ac22d9af
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-19 13:01:28 +0100

    manageUsers implementation

--- Sprong/inc/viewJobStatus.php
+++ Sprong/inc/viewJobStatus.php
@@ -1,24 +1,35 @@
-<!doctype html>
-<html>
-<meta charset="utf-8">
-<link rel="stylesheet" href="../CSS/style.css" type="text/css" />
-<body>
-
-
 <?php
 
-include("header.php");
+include ("connection.php");
+include ("header.php");
 
-echo "
+echo"
+
 <main>
-<p>viewJobStatus.php.</p>
-</main>
+<h2>View Job Progress</h2>
+<p>Below is a list of all work</p>
+<ul>
 ";
 
-include("footer.php");
+//  SQL query string to retrieve all job titles the database:
+$sql_query="SELECT * FROM jobs";
+
+//  Run the SQL query on the database:
+$result = mysqli_query($link,$sql_query);
+
+while($row = $result->fetch_array())
+{
+    $jid = $row['jid'];
+    $jobtitle = $row['jobtitle'];
+    $jobowner = $row['jobowner'];
+    echo "<li><a href='http://sprong.azurewebsites.net/Sprong/inc/viewJobStatus.php{$jid}'>{$jobtitle}</a> for {$jobowner}</li>";
+}
+
+echo "
+</main>"
+;
+
+include ("footer.php");
 
 ?>
 
-
-</body>
-</html>
