27aa219e9241f8bd4dea971a7b5fc58b5740627a
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-19 21:13:05 +0100

    Wednesday night

--- Sprong/inc/viewAllJobs.php
+++ Sprong/inc/viewAllJobs.php
@@ -22,7 +22,7 @@ echo"
 ";
 
 //  SQL query string to retrieve all job titles the database:
-$sql_query="SELECT jobnum, jobtitle, jobtext FROM jobs";
+$sql_query="SELECT DISTINCT jobnum, jobtitle, jobtext FROM jobs";
 
 //  Run the SQL query on the database:
 $result = mysqli_query($link,$sql_query);
