683803e000a00d1962db51022806672220db8a5e
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-20 12:49:24 +0100

    Wednesday night

--- Sprong/inc/viewJobProgress.php
+++ Sprong/inc/viewJobProgress.php
@@ -1,6 +1,6 @@
 <?php
 
-echo"<a href="http://sprong.azurewebsites.net/Sprong/inc/viewJobProgress.php"></a>"
+//  echo"<a href="http://sprong.azurewebsites.net/Sprong/inc/viewJobProgress.php"></a>"
 
 include ("connection.php");
 include ("header.php");
