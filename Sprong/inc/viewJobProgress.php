3f49dbdc743917920e4ec754b751f4e6470e1d16
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-20 13:19:35 +0100

    Wednesday night

--- Sprong/inc/viewJobProgress.php
+++ Sprong/inc/viewJobProgress.php
@@ -35,7 +35,7 @@ while($row = $result->fetch_array())
     $jobtext = $row['jobtext'];
     $jobstatus = $row['jobstatus'];
 
-    echo "<p>{$jobtext}" . "{$jobstatus}" . "{$jobstatus}</p>";
+    echo "<p>{$jobtext}" . "  Status: " . "{$jobstatus}</p>";
 }
 
 echo "</main>
