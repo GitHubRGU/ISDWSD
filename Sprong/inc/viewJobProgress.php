892765f9924a1b996375a13fcef8d2a1edf38277
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-20 20:27:47 +0100

    Thursday night

--- Sprong/inc/viewJobProgress.php
+++ Sprong/inc/viewJobProgress.php
@@ -2,7 +2,7 @@
 <html>
 <meta charset="utf-8">
 <link rel="stylesheet" href="http://sprong.azurewebsites.net/Sprong/CSS/style.css" type="text/css" />
-<?php include("./inc/background.html"); ?>
+<?php include("./Sprong/inc/background.html"); ?>
 <body>
 
 
