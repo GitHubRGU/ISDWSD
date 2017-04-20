1779336225992ae0c681b71aacbaf99a1fdd4677
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-20 22:05:10 +0100

    Thursday night

---
+++ Sprong/inc/setBackground.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<meta charset="utf-8">
+
+<style>
+    html {
+        background: url(../images/lpmbackground.jpg) no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+</style>
+
+</html>
