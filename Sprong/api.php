e32925b392370ced7ead23092d2b1dec1e035908
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-27 11:27:50 +0100

    Sprong API

--- Sprong/api.php
+++ Sprong/api.php
@@ -4,8 +4,6 @@
 //  ==========
 
 
-//  Get the HTTP method (GET, POST, PUT or DELETE) of the request ($method):
-$method = $_SERVER['REQUEST_METHOD'];
 
 //  Get the path of the request ($request):
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
@@ -15,8 +13,6 @@ $input = json_decode(file_get_contents('php://input'),true);
 
 //  Open a connection to the mySQL database ($link):
 include("inc/connection.php");
-//  IS THIS REQUIRED?:
-//  mysqli_set_charset($link,'utf8');
 
 //  Get the table and key (uid) from the path ($table and $key):
 $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
@@ -37,20 +33,13 @@ for ($i=0;$i<count($columns);$i++) {
 }
 
 //  Generate the SQL query, based on HTTP method ($sql):
-switch ($method) {
-    case 'GET':
-        $sql = "select * from `$table`".($key?" WHERE uid=$key":''); break;
-    case 'PUT':
-        $sql = "update `$table` set $set where uid=$key"; break;
-    case 'POST':
-        $sql = "insert into `$table` set $set"; break;
-    case 'DELETE':
-        $sql = "delete `$table` where uid=$key"; break;
-}
+$sql = "select * from `$table`".($key?" WHERE uid=$key":'');
+
 
 //  Execute the SQL query ($result):
 $result = mysqli_query($link,$sql);
 
+//  Error handling
 //   Drop the connection if the SQL query ($result) returns a 404 (Not Found) error:
 if (!$result) {
     http_response_code(404);
@@ -58,17 +47,11 @@ if (!$result) {
 }
 
 //  Create and display a JSON encoded table of the query results / inserted uid / number of affected rows:
-if ($method == 'GET') {
     if (!$key) echo '[';
     for ($i=0;$i<mysqli_num_rows($result);$i++) {
         echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
     }
     if (!$key) echo ']';
-} elseif ($method == 'POST') {
-    echo mysqli_insert_id($link);
-} else {
-    echo mysqli_affected_rows($link);
-}
 
 //  Close the database connection:
 mysqli_close($link);
