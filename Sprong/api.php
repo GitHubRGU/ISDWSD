276e383e5981e4f04aa0e08c48e0664db0404468
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-27 10:34:40 +0100

    Sprong API

--- Sprong/api.php
+++ Sprong/api.php
@@ -1,34 +1,42 @@
 <?php
 
-// get the HTTP method, path and body of the request
+//  Sprong API
+//  ==========
+
+
+//  Get the HTTP method (GET, POST, PUT or DELETE) of the request ($method):
 $method = $_SERVER['REQUEST_METHOD'];
+
+//  Get the path of the request ($request):
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+
+//  Get the body of the request ($input):
 $input = json_decode(file_get_contents('php://input'),true);
 
 //  Open a connection to the mySQL database ($link):
 include("inc/connection.php");
-//  REQUIRED?:
+//  IS THIS REQUIRED?:
 //  mysqli_set_charset($link,'utf8');
 
-// retrieve the table and key from the path
+//  Get the table and key (uid) from the path ($table and $key):
 $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 $key = array_shift($request)+0;
 
-// escape the columns and values from the input object
+//  Get the columns and values from the input object ($columns and $values):
 $columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
 $values = array_map(function ($value) use ($link) {
     if ($value===null) return null;
     return mysqli_real_escape_string($link,(string)$value);
 },array_values($input));
 
-// build the SET part of the SQL command
+//  Create the SET part of the SQL command (required for PUT or POST methods) ($set):
 $set = '';
 for ($i=0;$i<count($columns);$i++) {
     $set.=($i>0?',':'').'`'.$columns[$i].'`=';
     $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
 }
 
-// create SQL based on HTTP method
+//  Generate the SQL query, based on HTTP method ($sql):
 switch ($method) {
     case 'GET':
         $sql = "select * from `$table`".($key?" WHERE uid=$key":''); break;
@@ -40,16 +48,16 @@ switch ($method) {
         $sql = "delete `$table` where uid=$key"; break;
 }
 
-// excecute SQL statement
+//  Excecute the SQL query ($result):
 $result = mysqli_query($link,$sql);
 
-// die if SQL statement failed
+//   Drop the connection if the SQL query ($result) returned an error:
 if (!$result) {
     http_response_code(404);
     die(mysqli_error($link));
 }
 
-// print results, insert id or affected row count
+//  Display query results / the inserted uid or number of affected rows:
 if ($method == 'GET') {
     if (!$key) echo '[';
     for ($i=0;$i<mysqli_num_rows($result);$i++) {
@@ -62,5 +70,5 @@ if ($method == 'GET') {
     echo mysqli_affected_rows($link);
 }
 
-// close mysql connection
+//  Close the database connection:
 mysqli_close($link);
