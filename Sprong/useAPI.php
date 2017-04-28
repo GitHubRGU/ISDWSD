1f49de52ad8b7f408b69550193c82add7f0b9496
Shaun Hyland <s.hyland2@rgu.ac.uk>
2017-04-28 13:39:03 +0100

    Sprong API

--- Sprong/useAPI.php
+++ Sprong/useAPI.php
@@ -1,30 +1,24 @@
 <html>
 <head>
-    <link href="/css/style.css" rel="stylesheet" type="text/css" />
-    <title>PHP Bing</title>
+    <link href="./CSS/style.css" rel="stylesheet" type="text/css" />
+    <title>Use of Sprong API</title>
 </head>
-<body><form method="post" action="<?php echo $PHP_SELF;?>">
-    Type in a search:<input type="text" id="searchText" name="searchText" value="<?php
-    if (isset($_POST['searchText'])){
-        echo($_POST['searchText']); }
-    else { echo('sushi');}
-    ?>"/>
-    <input type="submit" value="Search!" name="submit" id="searchButton" />
+<body>
     <?php
-    if (isset($_POST['submit'])) {
-        $request = 'http://api.search.live.net/json.aspx?Appid=<YourAppIDHere>&sources=image&query=' . urlencode( $_POST["searchText"]);
+
+        $request = 'http://sprong.azurewebsites.net/Sprong/api.php/users';
         $response  = file_get_contents($request);
         $jsonobj  = json_decode($response);
         echo('<ul ID="resultList">');
 
-        foreach($jsonobj->SearchResponse->Image->Results as $value)
+        foreach($jsonobj->firstname->surname->email->usertype as $value)
         {
-            echo('<li class="resultlistitem"><a href="' . $value->Url . '">');
-            echo('<img src="' . $value->Thumbnail->Url. '"></li>');
+            echo('<li " . "$value"');
 
         }
         echo("</ul>");
-    } ?>
-</form>
+    }
+    ?>
+
 </body>
 </html>
